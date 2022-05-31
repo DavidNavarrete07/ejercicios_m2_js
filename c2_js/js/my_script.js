71878762a70e69a05540537f4a0cdf2df249798c
DavidNavarrete07 <davidalejandro20-09@outlook.es>
2022-05-31 14:27:55 -0400

    Realizando ejercicio bonus, piedra, papel o tijera

--- c2_js/js/my_script.js
+++ c2_js/js/my_script.js
@@ -33,3 +33,60 @@ edad = parseInt(prompt("JUBILACIÓN EN CHILE \n Ingrese su edad: "));
 // }
 // FORMA CORTA OPERADOR TERNARIO
 (genero === "hombre" && edad >= 65) ? alert("Usted tiene la edad necesaria (mayor a igual a 65) en los hombres para jubilarse en Chile") : (genero === "mujer" && edad >= 60) ? alert("Usted tiene la edad necesaria (mayor a igual a 60) en las mujeres para jubilarse en Chile") : alert("Aún no puede jubilarse");
+
+// Ejercicio bonus - Piedra, papel, tijera
+let eleccion_pc;
+let movimiento_pc;
+let eleccion_usuario;
+let movimiento_usuario;
+let respuesta_ganador;
+let contador = 1;
+const n_intentos = parseInt(prompt("PIEDRA, PAPEL O TIJERA \n Ingrese el número de intentos: "));
+do {
+    const aleatorio = Math.floor(Math.random() * 4);
+    (aleatorio === 0) ? Math.floor(Math.random() * 4) : eleccion_pc = aleatorio;
+    if (eleccion_pc === 1) {
+        movimiento_pc = "Piedra";
+    } else if (eleccion_pc === 2) {
+        movimiento_pc = "Papel";
+    } else if (eleccion_pc === 3) {
+        movimiento_pc = "Tijera";
+    } else {
+        movimiento_pc = "No válido"
+    }
+
+    eleccion_usuario = parseInt(prompt("PIEDRA, PAPEL O TIJERA \n Ingrese su elección: \n 1.- Piedra \n 2.- Papel \n 3.- Tijera"));
+    if (eleccion_usuario === 1) {
+        movimiento_usuario = "Piedra";
+    } else if (eleccion_usuario === 2) {
+        movimiento_usuario = "Papel";
+    } else if (eleccion_usuario === 3) {
+        movimiento_usuario = "Tijera";
+    } else {
+        movimiento_usuario = "No válido";
+        alert("Opción no válida, vuelva a intentar");
+    }
+
+    if (movimiento_pc === movimiento_usuario) {
+        alert("PC usó: " + movimiento_pc + "\n Usuario usó: " + movimiento_usuario + "\n Ha sido un empate");
+    } else if (movimiento_pc === "Tijera" && movimiento_usuario === "Papel") {
+        alert("PC usó: " + movimiento_pc + "\n Usuario usó: " + movimiento_usuario + "\n El ganador es el PC");
+    } else if (movimiento_pc === "Papel" && movimiento_usuario === "Piedra") {
+        alert("PC usó: " + movimiento_pc + "\n Usuario usó: " + movimiento_usuario + "\n El ganador es el PC");
+    } else if (movimiento_pc === "Piedra" && movimiento_usuario === "Tijera") {
+        alert("PC usó: " + movimiento_pc + "\n Usuario usó: " + movimiento_usuario + "\n El ganador es el PC");
+    } else if (movimiento_usuario === "Tijera" && movimiento_pc === "Papel") {
+        alert("PC usó: " + movimiento_pc + "\n Usuario usó: " + movimiento_usuario + "\n El ganador es el Usuario");
+    } else if (movimiento_usuario === "Papel" && movimiento_pc === "Piedra") {
+        alert("PC usó: " + movimiento_pc + "\n Usuario usó: " + movimiento_usuario + "\n El ganador es el Usuario");
+    } else if (movimiento_usuario === "Piedra" && movimiento_pc === "Tijera") {
+        alert("PC usó: " + movimiento_pc + "\n Usuario usó: " + movimiento_usuario + "\n El ganador es el Usuario");
+    } else {
+        alert("Opciones no válidas");
+    }
+    contador++;
+} while (contador <= n_intentos);
+alert("MUCHAS GRACIAS POR JUGAR!");
+
+
+
